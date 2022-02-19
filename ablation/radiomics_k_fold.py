d9844745dc03c97de393cd8acab82f7fc29e2438
chinmay5 <chinmayprabhakar55@gmail.com>
2022-02-19 12:45:57 +0100

    adding things for k fold run

--- ablation/radiomics_k_fold.py
+++ ablation/radiomics_k_fold.py
@@ -96,28 +96,80 @@ def work_on_combined_features(train_ids, test_ids, idx):
     return specificity, sensitivity
 
 
+def work_on_contrast_features(idx):
+    ssl_feature_dir = os.path.join(PROJECT_ROOT_DIR, 'brats', 'ssl_features_dir', 'all_comps')
+    train_features = np.load(os.path.join(ssl_feature_dir, f'train_contrast_ssl_features_split_{idx}.npy'))
+    test_features = np.load(os.path.join(ssl_feature_dir, f'test_contrast_ssl_features_split_{idx}.npy'))
+    train_labels = np.load(os.path.join(ssl_feature_dir, f'train_contrast_ssl_labels_split_{idx}.npy'))
+    test_labels = np.load(os.path.join(ssl_feature_dir, f'test_contrast_ssl_labels_split_{idx}.npy'))
+    # I found that normalizing features hurt performance in this case
+    radiomics_pred = classification(train_features=train_features, train_label=train_labels,
+                                    test_features=test_features)
+    radiomics_pred = radiomics_pred[:, 1]
+    specificity, sensitivity = evaluate_results(radiomics_pred=radiomics_pred, test_labels=test_labels)
+    return specificity, sensitivity
+
+
+def work_on_contrast_combined_features(train_ids, test_ids, idx):
+    ssl_feature_dir = os.path.join(PROJECT_ROOT_DIR, 'brats', 'ssl_features_dir', 'all_comps')
+    train_features = np.load(os.path.join(ssl_feature_dir, f'train_contrast_ssl_features_split_{idx}.npy'))
+    test_features = np.load(os.path.join(ssl_feature_dir, f'test_contrast_ssl_features_split_{idx}.npy'))
+    ssl_train_labels = np.load(os.path.join(ssl_feature_dir, f'train_contrast_ssl_labels_split_{idx}.npy'))
+    ssl_test_labels = np.load(os.path.join(ssl_feature_dir, f'test_contrast_ssl_labels_split_{idx}.npy'))
+    radiomics_dir = '/mnt/cat/chinmay/brats_processed'
+    radiomics_features = np.load(os.path.join(radiomics_dir, 'data', 'radiomics_features', 'features_flair.npy'))
+    all_labels = np.load(os.path.join(radiomics_dir, 'label_all.npy'))
+    radiomics_train_feat, train_labels = radiomics_features[train_ids], all_labels[train_ids]
+    radiomics_test_features, test_labels = radiomics_features[test_ids], all_labels[test_ids]
+    # Concatenate the features
+    assert np.all(train_labels == ssl_train_labels), "Something wrong with train labels"
+    assert np.all(test_labels == ssl_test_labels), "Something wrong with test labels"
+    train_features = np.concatenate((radiomics_train_feat, train_features), axis=1)
+    test_features = np.concatenate((radiomics_test_features, test_features), axis=1)
+    # noramlize the features
+    normalize_features(features=train_features)
+    normalize_features(features=test_features)
+    radiomics_pred = classification(train_features=train_features, train_label=train_labels,
+                                    test_features=test_features)
+    radiomics_pred = radiomics_pred[:, 1]
+    specificity, sensitivity = evaluate_results(radiomics_pred=radiomics_pred, test_labels=test_labels)
+    return specificity, sensitivity
+
+
+
 def evaluate_features():
     # At this moment, the evaluation is for Brats only.
     split_index_path = os.path.join(PROJECT_ROOT_DIR, 'brats', 'k_fold', 'indices_file')
     avg_specificity, avg_sensitivity, avg_sensitivity_ssl, avg_specificity_ssl, avg_specificity_comb, avg_sensitivity_comb = 0, 0, 0, 0, 0, 0
+    avg_sensitivity_ssl_contr, avg_specificity_ssl_contr, avg_specificity_comb_contr, avg_sensitivity_comb_contr = 0, 0, 0, 0
     for idx in range(3):
         train_ids = pickle.load(open(os.path.join(split_index_path, f"train_{idx}"), 'rb'))
         test_ids = pickle.load(open(os.path.join(split_index_path, f"test_{idx}"), 'rb'))
         specificity, sensitivity = work_on_radiomics_features(train_ids=train_ids, test_ids=test_ids)
         specificity_ssl, sensitivity_ssl = work_on_ssl_features(idx=idx)
         specificity_ssl_comb, sensitivity_ssl_comb = work_on_combined_features(train_ids=train_ids, test_ids=test_ids, idx=idx)
+        specificity_ssl_contr, sensitivity_ssl_contr = work_on_contrast_features(idx=idx)
+        specificity_ssl_comb_contr, sensitivity_ssl_comb_contr = work_on_contrast_combined_features(train_ids=train_ids, test_ids=test_ids, idx=idx)
         avg_sensitivity += sensitivity
         avg_specificity += specificity
         avg_sensitivity_ssl += sensitivity_ssl
         avg_specificity_ssl += specificity_ssl
         avg_sensitivity_comb += sensitivity_ssl_comb
         avg_specificity_comb += specificity_ssl_comb
+        avg_specificity_ssl_contr += specificity_ssl_contr
+        avg_sensitivity_ssl_contr += sensitivity_ssl_contr
+        avg_specificity_comb_contr += specificity_ssl_comb_contr
+        avg_sensitivity_comb_contr += sensitivity_ssl_comb_contr
     print("Radiomics features")
     print(f"Average specificity {avg_specificity/3} and sensitivity {avg_sensitivity/3}")
     print("SSL Features")
     print(f"Average specificity {avg_specificity_ssl/3} and sensitivity {avg_sensitivity_ssl/3}")
     print("Combined Features")
     print(f"Average specificity {avg_specificity_comb / 3} and sensitivity {avg_sensitivity_comb / 3}")
+    print("Contrast Features")
+    print(f"Average specificity {avg_specificity_ssl_contr / 3} and sensitivity {avg_sensitivity_ssl_contr / 3}")
+    print("Combined Features")
+    print(f"Average specificity {avg_specificity_comb_contr / 3} and sensitivity {avg_sensitivity_comb_contr / 3}")
 
 
 if __name__ == '__main__':
--- visualization/tsne_plots.py
+++ visualization/tsne_plots.py
@@ -1,15 +1,116 @@
 import os
+import pickle
 
 import numpy as np
+import torch
 from sklearn.manifold import TSNE
 from matplotlib import pyplot as plt
 from sklearn.preprocessing import OneHotEncoder
 
+from dataset.dataset_factory import get_dataset
 from environment_setup import PROJECT_ROOT_DIR
+from fine_tune.contrastive_training import get_args_parser
+from model.model_factory import get_models
+from model.model_utils.vit_helpers import interpolate_pos_embed
+from read_configs import bootstrap
 
 base_dir = '/mnt/cat/chinmay/brats_processed'
 radiomics_path = os.path.join(base_dir, 'data', 'radiomics_features', 'features_flair.npy')
 labels_path = os.path.join(base_dir, 'label_all.npy')
+
+
+def load_perceptual_model(model_perc, args, device):
+    args.finetune = os.path.join(PROJECT_ROOT_DIR, args.common_path, args.checkpoint_perc)
+    checkpoint = torch.load(args.finetune, map_location='cpu')
+
+    print("Load pre-trained checkpoint from: %s" % args.finetune)
+    checkpoint_model = checkpoint['model']
+    state_dict = model_perc.state_dict()
+    for k in ['head.weight', 'head.bias']:
+        if k in checkpoint_model and checkpoint_model[k].shape != state_dict[k].shape:
+            print(f"Removing key {k} from pretrained checkpoint")
+            del checkpoint_model[k]
+
+    # interpolate position embedding
+    interpolate_pos_embed(model_perc, checkpoint_model)
+
+    # load pre-trained model
+    msg = model_perc.load_state_dict(checkpoint_model, strict=False)
+    print(msg)
+
+    model_perc.to(device)
+    if args.global_pool:
+        assert set(msg.missing_keys) == {'head.weight', 'head.bias', 'fc_norm.weight', 'fc_norm.bias'}
+    else:
+        assert set(msg.missing_keys) == {'head.weight', 'head.bias'}
+
+
+def extract_contrast_feat():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    args = get_args_parser()
+    args = args.parse_args()
+    args = bootstrap(args=args, key='TSNE')
+    dataset_train = get_dataset(dataset_name=args.dataset, mode='train', args=args, transforms=None,
+                                use_z_score=args.use_z_score)
+    dataset_test = get_dataset(dataset_name=args.dataset, mode='test', args=args, transforms=None,
+                               use_z_score=args.use_z_score)
+
+    data_loader_test = torch.utils.data.DataLoader(
+        dataset_test,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+        pin_memory=args.pin_mem,
+        drop_last=False,
+    )
+
+    model_perc = get_models(model_name='vit', args=args)
+    # Small change needed to load the contrastive model
+    args.nb_classes = -1
+    model_contrast = get_models(model_name='contrastive', args=args)
+    # Let us now load the model weights for the two different models
+    print("Loading contrastive model")
+    load_contrast_model(args, model_contrast)
+    print("Loading perceptual model")
+    load_perceptual_model(model_perc=model_perc, args=args, device=device)
+
+    model_contrast.eval()
+    model_perc.eval()
+    # Boiler-plate for storing the features
+    outPRED_CON = torch.FloatTensor().to(device)
+    outPRED_AUG_CON = torch.FloatTensor().to(device)
+    # Same for perceptual
+    outPRED_PER = torch.FloatTensor().to(device)
+    outPRED_AUG_PER = torch.FloatTensor().to(device)
+    for data_iter_step, (augmented, original, _) in enumerate(data_loader_test):
+
+        augmented = augmented.to(device, non_blocking=True)
+        original = original.to(device, non_blocking=True)
+        with torch.cuda.amp.autocast():
+            orig_feat_contrast = model_contrast.forward_features(original)
+            aug_feat_contrast = model_contrast.forward_features(augmented)
+            # Also for the perceptual features
+            orig_feat_perc = model_perc.forward_features(original)
+            aug_feat_perc = model_perc.forward_features(augmented)
+        # Now augment all the features
+        outPRED_CON = torch.cat((outPRED_CON, orig_feat_contrast), 0)
+        outPRED_AUG_CON = torch.cat((outPRED_AUG_CON, aug_feat_contrast), 0)
+        outPRED_PER = torch.cat((outPRED_PER, orig_feat_perc), 0)
+        outPRED_AUG_PER = torch.cat((outPRED_AUG_PER, aug_feat_perc), 0)
+
+    # Let us now save all the features that we have generated for the test set
+    feature_dir = os.path.join(PROJECT_ROOT_DIR, 'visualization')
+    np.save(os.path.join(feature_dir, 'cont_orig.npy'), outPRED_CON.cpu().numpy())
+    np.save(os.path.join(feature_dir, 'cont_aug.npy'), outPRED_AUG_CON.cpu().numpy())
+    np.save(os.path.join(feature_dir, 'perc_orig.npy'), outPRED_PER.cpu().numpy())
+    np.save(os.path.join(feature_dir, 'perc_aug.npy'), outPRED_AUG_PER.cpu().numpy())
+
+
+
+def load_contrast_model(args, model_contrast):
+    model_path = os.path.join(PROJECT_ROOT_DIR, args.common_path, args.checkpoint_contr)
+    assert os.path.exists(model_path), "Please ensure a trained model alredy exists"
+    checkpoint = torch.load(model_path, map_location='cpu')
+    model_contrast.load_state_dict(checkpoint['model'])
 
 
 def create_tsne_plot(features, labels, plot_name):
@@ -53,6 +154,13 @@ def plot_figs():
 
 
 def get_radiomics_feat():
+    split_index_path = os.path.join(PROJECT_ROOT_DIR, 'brats', 'k_fold', 'indices_file')
+    idx = 1
+    test_ids = pickle.load(open(os.path.join(split_index_path, f"test_{idx}"), 'rb'))
+
+
+
+
     radiomics = np.asarray(np.load(radiomics_path))
     labels = np.load(labels_path)
     # train_idx = np.load(os.path.join(base_dir, 'data', 'train_indices.npy'))
@@ -61,4 +169,4 @@ def get_radiomics_feat():
     return train_X_rad_, train_y_
 
 if __name__ == '__main__':
-    plot_figs()
+    extract_contrast_feat()
